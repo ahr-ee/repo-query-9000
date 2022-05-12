b5e6a10ce63fb1e8e3da6f7891ec7cf59cd327eb
ahr-ee <ultra.awesome.stuff@gmail.com>
2022-05-11 23:30:33 -0400

    adding about page

--- fe/src/pages/About.js
+++ fe/src/pages/About.js
@@ -3,7 +3,13 @@ import React from "react";
 const About = () => {
 
     return (
-        <div><h1> ABOOOOT </h1></div>
+        <div>
+            <h1> ABOUT THE DEV </h1>
+            <div> Name: Arianna </div>
+            <div> Hobbies: Rock Climbing, Video Games, Drums, Music Production, Snowboarding </div>
+            <div> Links: <a href="https://soundcloud.com/bridge-enthusiast" rel="noreferrer">SoundCloud</a>, <a href="https://www.youtube.com/channel/UCG9lBidLHBxAztSOzLtfAcw" rel="noreferrer">YouTube</a></div>
+            
+        </div>
     )
 }
 
